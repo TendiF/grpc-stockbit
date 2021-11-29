65ed957f0c24cfdcc00572a6826eff9ffeaa523f
tendif <tendifirmansyah30@gmail.com>
2021-11-29 09:32:01 +0700

    add logs

--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
 	"google.golang.org/grpc"
 )
@@ -11,8 +13,17 @@ func serverInterceptor(ctx context.Context,
 	req interface{},
 	info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler) (interface{}, error) {
-	s := fmt.Sprintf("%s : %s", info.FullMethod, req)
-	fmt.Println("zz", s)
+	s := fmt.Sprintf("%s|%s", info.FullMethod, req)
+
+	file, err := os.OpenFile("./tmp/logs.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.SetOutput(file)
+
+	log.Println("ACCESS|" + s)
+
 	// Calls the handler
 	h, err := handler(ctx, req)
 
